f4bba98a849b1a5dcb0b2602b186b0f4372cd70b
Michał Młynarczyk <>
2024-07-01 21:54:15 +0200

    logs

--- script.sh
+++ script.sh
@@ -4,8 +4,18 @@ show_date(){
  echo "$(date)"
 }
 
+create_logs(){
+  for i in {1..100}
+  do
+	  echo "log${i}.txt $0 $(date)" > "log${i}.txt"
+  done
+}
+
 case "$1" in
 	--date)
 		show_date
+		;;
+	--logs)
+		create_logs
 		;;
 esac
