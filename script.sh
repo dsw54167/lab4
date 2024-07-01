f9d8cf059a35610d97b52caaf7c6df36d1e090be
Michał Młynarczyk <>
2024-07-01 22:23:05 +0200

    add logs argument

--- script.sh
+++ script.sh
@@ -5,17 +5,23 @@ show_date(){
 }
 
 create_logs(){
-  for i in {1..100}
+  local -i max=${1:-100}
+  for i in $(seq 1 $max);
   do
-	  echo "log${i}.txt $0 $(date)" > "log${i}.txt"
+    echo "log${i}.txt $0 $(date)" > "log${i}.txt"
   done
 }
 
 case "$1" in
-	--date)
-		show_date
-		;;
-	--logs)
-		create_logs
-		;;
+  --date)
+    show_date
+  ;;
+  --logs)
+    if [ ! -z $2 ];
+    then
+      create_logs $2
+    else
+      create_logs
+    fi
+      ;;
 esac
